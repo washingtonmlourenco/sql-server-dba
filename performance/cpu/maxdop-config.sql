a474536a2a177f25feb94addcbc5c154dc695dbf
Washington M. Lourenço <67602627+washingtonmlourenco@users.noreply.github.com>
2026-02-15 21:14:56 -0300

    Update maxdop-config.sql

--- performance/cpu/maxdop-config.sql
+++ performance/cpu/maxdop-config.sql
@@ -1,1 +1,11 @@
-
+SELECT @@SERVERNAME InstanceName
+	,SERVERPROPERTY('ComputerNamePhysicalNetBIOS') Hostname
+	,SERVERPROPERTY('ProductVersion') AS ProductVersion
+	,cpu_count
+	,hyperthread_ratio
+	,softnuma_configuration
+	,softnuma_configuration_desc
+	,socket_count
+	,numa_node_count
+	,(cpu_count / numa_node_count) ReferenceNumberForMaxDop
+FROM sys.dm_os_sys_info
